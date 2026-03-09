c2992244cf1960543e739e7222443aee15ed357f
Aakash Kathole <aakashkatholeak@gmail.com>
2026-03-09 12:40:32 +0530

    chore: convert to JS, setup folder structure and install dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@api': './src/api',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@navigation': './src/navigation',
+          '@hooks': './src/hooks',
+          '@services': './src/services',
+          '@utils': './src/utils',
+          '@constants': './src/constants',
+          '@context': './src/context',
+          '@assets': './src/assets',
+          '@store': './src/store',
+          '@models': './src/models',
+        },
+      },
+    ],
+  ],
 };
