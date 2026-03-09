52d5125ad73d476283744e1e9cd497dc954e6a5b
Aakash Kathole <aakashkatholeak@gmail.com>
2026-03-09 14:39:39 +0530

    setup poppins font

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 const App = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>PJSoftTech School</Text>
+      <Text style={{ fontFamily: 'Poppins-Regular'}}>PJSoftTech School</Text>
     </View>
   );
 };
