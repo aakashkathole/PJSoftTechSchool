b99997eee76b732f893ca5b7537d32beee5566d1
Aakash Kathole <aakashkatholeak@gmail.com>
2026-03-18 14:43:43 +0530

    migrate package name, signing config and folder structure from old project

--- android/app/src/main/java/com/pjsofttech/schoolteacherapp/MainActivity.kt
+++ android/app/src/main/java/com/pjsofttech/schoolteacherapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.pjsofttechschool
+package com.pjsofttech.schoolteacherapp
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/pjsofttech/schoolteacherapp/MainApplication.kt
+++ android/app/src/main/java/com/pjsofttech/schoolteacherapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.pjsofttechschool
+package com.pjsofttech.schoolteacherapp
 
 import android.app.Application
 import com.facebook.react.PackageList
